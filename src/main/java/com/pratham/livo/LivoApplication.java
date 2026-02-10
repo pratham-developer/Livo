d79e9feaa97070510054abb24f542b1ce7b07ca1
Pratham Khanduja <prathk30@gmail.com>
2026-02-11 03:32:33 +0530

    fix timezone for deployment

--- src/main/java/com/pratham/livo/LivoApplication.java
+++ src/main/java/com/pratham/livo/LivoApplication.java
@@ -1,12 +1,9 @@
 package com.pratham.livo;
 
-import jakarta.annotation.PostConstruct;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,12 +13,4 @@ public class LivoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LivoApplication.class, args);
     }
-
-    @PostConstruct
-    public void init() {
-        //run app in IST
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
-    }
-    
-
 }
